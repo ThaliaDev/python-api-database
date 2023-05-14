0c2f8b6ab4eb1e63315a26e2d7de25c97ce1944d
Thalia Rodrigues <thalia_sara@hotmail.com>
2023-05-14 19:35:17 -0300

    db

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-import database
 import apsw
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
 
-database_name = 'teste.db'
+database_name = 'server.db'
 tablename_name = "servidores"
 
 conn = apsw.Connection(database_name)
 c = conn.cursor()
+
 
 class Urls(Resource):
     def post(self):
         data = request.get_json()
         url = data['url']
         sigla = data['sigla']
-        database.insert_table(c, data)
+        c.execute("INSERT INTO servidores (url, sigla) VALUES (?,?)",
+                  (data['url'], data['sigla']))
         return {'url': url, 'sigla': sigla}, 201
 
     def get(self):
@@ -33,9 +33,11 @@ class Urls(Resource):
             urls.append({'id': row[0], 'url': row[1], 'sigla': row[2]})
         return {'urls': urls}
 
+
 api.add_resource(Urls, '/urls')
 
 if __name__ == '__main__':
     if conn.table_exists(database_name, tablename_name):
-        database.create_table(c)
+        c.execute(
+            f"CREATE TABLE {tablename_name} (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, url TEXT NOT NULL, sigla TEXT NOT NULL);")
     app.run(debug=True)
